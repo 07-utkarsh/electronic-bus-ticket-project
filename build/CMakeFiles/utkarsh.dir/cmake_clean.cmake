1db554a5509bbd843af29619681112945f1b51d8
jabedzaman <jabedzaman2004@gmail.com>
2023-05-02 09:51:52 +0530

    (fix) update error

---
+++ build/CMakeFiles/utkarsh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utkarsh.dir/main.c.o"
+  "CMakeFiles/utkarsh.dir/main.c.o.d"
+  "utkarsh"
+  "utkarsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/utkarsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
